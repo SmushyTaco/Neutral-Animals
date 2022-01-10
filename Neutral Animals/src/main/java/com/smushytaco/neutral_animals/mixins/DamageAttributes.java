deef26f8e3a5244bcd22ce944eeb512af645a157
SmushyTaco <smushytaco@gmail.com>
2022-01-10 00:56:38 -0800

    Formatting and Version Bump

--- Neutral Animals/src/main/java/com/smushytaco/neutral_animals/mixins/DamageAttributes.java
+++ Neutral Animals/src/main/java/com/smushytaco/neutral_animals/mixins/DamageAttributes.java
@@ -3,7 +3,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.AttributeContainer;
@@ -15,16 +14,13 @@ import net.minecraft.entity.passive.PigEntity;
 import net.minecraft.entity.passive.RabbitEntity;
 import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.world.World;
-
 @Mixin(LivingEntity.class)
 public abstract class DamageAttributes {
 	@Inject(method = "<init>", at = @At("TAIL"))
 	private void init(EntityType<? extends LivingEntity> entityType, World world, CallbackInfo info) {
-		LivingEntity livingEntity = (LivingEntity)(Object)this;
+		LivingEntity livingEntity = (LivingEntity) (Object) this;
 		AttributeContainer container = livingEntity.getAttributes();
-		
-		if(!container.hasAttribute(EntityAttributes.GENERIC_ATTACK_DAMAGE) && (livingEntity instanceof ChickenEntity || livingEntity instanceof CowEntity || livingEntity instanceof PigEntity || livingEntity instanceof RabbitEntity || livingEntity instanceof SheepEntity)) {
-			((AttributeContainerAccess)container).getCustom().putIfAbsent(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeInstance(EntityAttributes.GENERIC_ATTACK_DAMAGE, (it) -> {}));
-		}
+		if (container.hasAttribute(EntityAttributes.GENERIC_ATTACK_DAMAGE) || (!(livingEntity instanceof ChickenEntity) && !(livingEntity instanceof CowEntity) && !(livingEntity instanceof PigEntity) && !(livingEntity instanceof RabbitEntity) && !(livingEntity instanceof SheepEntity))) return;
+		((AttributeContainerAccess) container).getCustom().putIfAbsent(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeInstance(EntityAttributes.GENERIC_ATTACK_DAMAGE, (it) -> {}));
 	}
 }
