852b7a8e778b6b2abe7e1d05bd6b811a41fcd510
Nikan Radan <github.com@alias.nikanradan.com>
2023-06-18 22:56:05 -0700

    1.20.1

--- Neutral Animals/src/main/java/com/smushytaco/neutral_animals/mixins/ChickenEntityToNeutral.java
+++ Neutral Animals/src/main/java/com/smushytaco/neutral_animals/mixins/ChickenEntityToNeutral.java
@@ -3,61 +3,29 @@ import com.smushytaco.neutral_animals.NeutralAnimals;
 import com.smushytaco.neutral_animals.angerable_defaults.DefaultAngerable;
 import com.smushytaco.neutral_animals.angerable_defaults.DefaultAngerableValues;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.ChickenEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ChickenEntity.class)
 public abstract class ChickenEntityToNeutral extends AnimalEntity implements DefaultAngerable {
+    protected ChickenEntityToNeutral(EntityType<? extends AnimalEntity> entityType, World world) { super(entityType, world); }
+    @Unique
     DefaultAngerableValues defaultAngerableValues = new DefaultAngerableValues();
-    protected ChickenEntityToNeutral(EntityType<? extends AnimalEntity> entityType, World world) {
-        super(entityType, world);
-    }
-    @Inject(method = "initGoals", at = @At("RETURN"))
-    @SuppressWarnings("all")
-    private void hookInitGoals(CallbackInfo ci) {
-        if (!NeutralAnimals.INSTANCE.getConfig().getChickensAreNeutral()) return;
-        NeutralAnimals.INSTANCE.neutralAnimalGoalAndTargets(goalSelector, targetSelector, (ChickenEntity & DefaultAngerable) (Object) this);
-    }
     @NotNull
     @Override
     public DefaultAngerableValues getDefaultAngerableValues() { return defaultAngerableValues; }
-    @Inject(method = "writeCustomDataToNbt", at = @At("RETURN"))
-    private void hookWriteCustomDataToTag(NbtCompound nbt, CallbackInfo ci) {
-        if (!NeutralAnimals.INSTANCE.getConfig().getChickensAreNeutral()) return;
-        writeAngerToNbt(nbt);
-    }
-    @Inject(method = "readCustomDataFromNbt", at = @At("RETURN"))
-    private void hookReadCustomDataFromTag(NbtCompound nbt, CallbackInfo ci) {
-        if (!NeutralAnimals.INSTANCE.getConfig().getChickensAreNeutral()) return;
-        readAngerFromNbt(world, nbt);
-    }
-    @Override
+    @Inject(method = "initGoals", at = @At("RETURN"))
     @SuppressWarnings("all")
-    protected void mobTick() {
-        if (NeutralAnimals.INSTANCE.getConfig().getChickensAreNeutral()) {
-            NeutralAnimals.INSTANCE.mobTickLogic((ChickenEntity & DefaultAngerable) (Object) this);
-        }
-        super.mobTick();
-    }
-    @Override
-    public void setTarget(@Nullable LivingEntity target) {
-        if (NeutralAnimals.INSTANCE.getConfig().getChickensAreNeutral()) {
-            if (getTarget() == null && target != null) {
-                defaultAngerableValues.setAngerPassingCooldown(NeutralAnimals.INSTANCE.getANGER_PASSING_COOLDOWN_RANGE().get(random));
-            }
-            if (target instanceof PlayerEntity) {
-                setAttacking((PlayerEntity) target);
-            }
-        }
-        super.setTarget(target);
-    }
+    private void hookInitGoals(CallbackInfo ci) { if (NeutralAnimals.INSTANCE.getConfig().getChickensAreNeutral()) NeutralAnimals.INSTANCE.neutralAnimalGoalAndTargets(goalSelector, targetSelector, (ChickenEntity & DefaultAngerable) (Object) this); }
+    @Inject(method = "writeCustomDataToNbt", at = @At("RETURN"))
+    private void hookWriteCustomDataToTag(NbtCompound nbt, CallbackInfo ci) { if (NeutralAnimals.INSTANCE.getConfig().getChickensAreNeutral()) writeAngerToNbt(nbt); }
+    @Inject(method = "readCustomDataFromNbt", at = @At("RETURN"))
+    private void hookReadCustomDataFromTag(NbtCompound nbt, CallbackInfo ci) { if (NeutralAnimals.INSTANCE.getConfig().getChickensAreNeutral()) readAngerFromNbt(getWorld(), nbt); }
 }
--- Neutral Animals/src/main/java/com/smushytaco/neutral_animals/mixins/CowEntityToNeutral.java
+++ Neutral Animals/src/main/java/com/smushytaco/neutral_animals/mixins/CowEntityToNeutral.java
@@ -1,65 +1,27 @@
 package com.smushytaco.neutral_animals.mixins;
+
 import com.smushytaco.neutral_animals.NeutralAnimals;
 import com.smushytaco.neutral_animals.angerable_defaults.DefaultAngerable;
 import com.smushytaco.neutral_animals.angerable_defaults.DefaultAngerableValues;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.CowEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(CowEntity.class)
 public abstract class CowEntityToNeutral extends AnimalEntity implements DefaultAngerable {
+    protected CowEntityToNeutral(EntityType<? extends AnimalEntity> entityType, World world) { super(entityType, world); }
+    @Unique
     DefaultAngerableValues defaultAngerableValues = new DefaultAngerableValues();
-    protected CowEntityToNeutral(EntityType<? extends AnimalEntity> entityType, World world) {
-        super(entityType, world);
-    }
-    @Inject(method = "initGoals", at = @At("RETURN"))
-    @SuppressWarnings("all")
-    private void hookInitGoals(CallbackInfo ci) {
-        if (!NeutralAnimals.INSTANCE.getConfig().getCowsAreNeutral()) return;
-        NeutralAnimals.INSTANCE.neutralAnimalGoalAndTargets(goalSelector, targetSelector, (CowEntity & DefaultAngerable) (Object) this);
-    }
     @NotNull
     @Override
     public DefaultAngerableValues getDefaultAngerableValues() { return defaultAngerableValues; }
-    @Override
-    public void writeCustomDataToNbt(NbtCompound nbt) {
-        super.writeCustomDataToNbt(nbt);
-        if (!NeutralAnimals.INSTANCE.getConfig().getCowsAreNeutral()) return;
-        writeAngerToNbt(nbt);
-    }
-    @Override
-    public void readCustomDataFromNbt(NbtCompound nbt) {
-        super.readCustomDataFromNbt(nbt);
-        if (!NeutralAnimals.INSTANCE.getConfig().getCowsAreNeutral()) return;
-        readAngerFromNbt(world, nbt);
-    }
-    @Override
+    @Inject(method = "initGoals", at = @At("RETURN"))
     @SuppressWarnings("all")
-    protected void mobTick() {
-        if (NeutralAnimals.INSTANCE.getConfig().getCowsAreNeutral()) {
-            NeutralAnimals.INSTANCE.mobTickLogic((CowEntity & DefaultAngerable) (Object) this);
-        }
-        super.mobTick();
-    }
-    @Override
-    public void setTarget(@Nullable LivingEntity target) {
-        if (NeutralAnimals.INSTANCE.getConfig().getCowsAreNeutral()) {
-            if (getTarget() == null && target != null) {
-                defaultAngerableValues.setAngerPassingCooldown(NeutralAnimals.INSTANCE.getANGER_PASSING_COOLDOWN_RANGE().get(random));
-            }
-            if (target instanceof PlayerEntity) {
-                setAttacking((PlayerEntity) target);
-            }
-        }
-        super.setTarget(target);
-    }
+    private void hookInitGoals(CallbackInfo ci) { if (NeutralAnimals.INSTANCE.getConfig().getCowsAreNeutral()) NeutralAnimals.INSTANCE.neutralAnimalGoalAndTargets(goalSelector, targetSelector, (CowEntity & DefaultAngerable) (Object) this); }
 }
